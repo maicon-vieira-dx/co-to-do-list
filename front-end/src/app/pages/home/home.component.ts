5c56194b738506e0e1c8aab912f01b238e9492ab
AKRKPROMOTORA\maicon.costa <maicon.costa@tecksolucoes.com.br>
2025-10-14 16:44:37 -0300

    feat: Adicionando listagem de tarefas da API

--- front-end/src/app/pages/home/home.component.ts
+++ front-end/src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component, Signal, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { SharedMaterialModule } from '../../shared/material/shared-material.module';
 import { HeaderHomeComponent } from './header-home/header-home.component';
 import { SidebarHomeComponent } from './sidebar-home/sidebar-home.component';
-import { Item, Priority, Status } from '@app/model/item.model';
+import { Item } from '@app/model/item.model';
 import { CardHomeComponent } from "./card-home/card-home.component";
+import { TaskService } from '@app/services/task.service';
 
 @Component({
   selector: 'app-home',
@@ -12,45 +13,16 @@ import { CardHomeComponent } from "./card-home/card-home.component";
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  tasks = signal<Item[]>([
-    {
-      id: '1',
-      title: 'Implementar sistema de autenticação',
-      description: 'Criar módulo de login e registro com JWT token',
-      status: Status.IN_PROGRESS,
-      priority: Priority.HIGH,
-      createdAt: new Date('2024-01-15T10:30:00'),
-      dueDate: new Date('2024-01-25T18:00:00'),
-      category: 'Desenvolvimento',
-      tags: ['frontend', 'backend', 'segurança'],
-      isActive: true
-    },
-    {
-      id: '2',
-      title: 'Reunião de planejamento do sprint',
-      description: 'Definir metas e tarefas para o próximo sprint com a equipe',
-      status: Status.PENDING,
-      priority: Priority.MEDIUM,
-      createdAt: new Date('2024-01-18T14:00:00'),
-      dueDate: new Date('2024-01-22T09:00:00'),
-      category: 'Reunião',
-      tags: ['planejamento', 'equipe', 'metas'],
-      isActive: false
-    },
-    {
-      id: '3',
-      title: 'Corrigir bug no carrinho de compras',
-      description:
-        'O valor total não está atualizando quando itens são removidos',
-      status: Status.CANCELLED,
-      priority: Priority.URGENT,
-      createdAt: new Date('2024-01-19T08:15:00'),
-      dueDate: new Date('2024-01-20T12:00:00'),
-      category: 'Bug Fix',
-      tags: ['bug', 'crítico', 'ecommerce'],
-      isActive: false
-    },
-  ]);
+  tasks = signal<Item[]>([]);
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit() {
+    this.taskService.getTasks().subscribe({
+      next: (res) => this.tasks.set(res),
+      error: (err) => console.error('Erro ao carregar tarefas:', err)
+    })
+  };
 
   activeTask(id: string) {
     this.tasks.update(tasks =>
@@ -59,5 +31,5 @@ export class HomeComponent {
         isActive: task.id === id
       }))
     );
-  }
+  };
 }
