15445b9b8a68688eb7d7626a351e0e06a009dd1f
AKRKPROMOTORA\maicon.costa <maicon.costa@tecksolucoes.com.br>
2025-10-15 12:09:40 -0300

    feat: Biblioteca para exibir erros de validação

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -1,10 +1,27 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideErrorTailorConfig } from '@ngneat/error-tailor';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { MaterialErrorComponent } from './components/material-error/material-error.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideHttpClient(withFetch())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+    provideErrorTailorConfig({
+      errors: {
+        useValue: {
+          required: () => 'Campo obrigatório',
+          maxlength: ({ requiredLength, actualLength }) =>
+            `Máximo ${requiredLength} caracteres (${actualLength} usados)`,
+          invalidAddress: (error) => `Address isn't valid`,
+        },
+      },
+      controlErrorComponent: MaterialErrorComponent
+    }),
+  ],
 };
--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { EditarComponent } from './pages/editar/editar.component';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CriarComponent } from './pages/criar/criar.component';
@@ -5,6 +6,6 @@ import { CriarComponent } from './pages/criar/criar.component';
 export const routes: Routes = [
     { path: "", component: HomeComponent },
     { path: "criar", component: CriarComponent },
-    { path: "editar", component: CriarComponent },
+    { path: "editar", component: EditarComponent },
     { path: "**", redirectTo: "" }
 ];
