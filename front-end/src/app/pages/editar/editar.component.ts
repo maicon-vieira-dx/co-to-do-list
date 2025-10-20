85852d4e35af1e46efffcd77d8c5805e2fcd9f79
AKRKPROMOTORA\maicon.costa <maicon.costa@tecksolucoes.com.br>
2025-10-20 17:17:21 -0300

    refactor: Ajustando tags para o formulário

--- front-end/src/app/pages/editar/editar.component.ts
+++ front-end/src/app/pages/editar/editar.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal, WritableSignal } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HeaderComponent } from '@app/components/header/header.component';
 import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
 import { TaskFormComponent } from '@app/components/task-form/task-form.component';
+import { Task } from '@app/model/task.model';
 import { TaskStore } from '@app/services/store/task.store';
 import { ApiResponse } from '@app/shared/types/api';
 import { ToastrService } from 'ngx-toastr';
@@ -16,35 +17,43 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class EditarComponent {
   protected form!: FormGroup;
-  protected tags = signal<{ id: number; name: string }[]>([]);
   protected errors: WritableSignal<ApiResponse["errors"]> = signal([]);
+  protected task = signal<Task | null>(null);
 
-  constructor(private fb: FormBuilder, public taskStore: TaskStore, private router: Router, private toastr: ToastrService) { }
+  constructor(private fb: FormBuilder, private router: Router, private toastr: ToastrService, private taskStore: TaskStore, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.tags.set(this.taskStore.taskSelected()?.tags.map((name, id) => ({ name, id })) || []);
     this.form = this.createForm();
+    this.route.queryParamMap.subscribe(params => {
+      this.taskStore.readById(params?.get("id"))
+      .then(({ title, status, description, priority, category, tags = [] }) => {
+        this.form.patchValue({ title, status, description, priority, category, tags });
+        tags.forEach((tag: string) => {
+          (this.form.get('tags') as FormArray).push(this.fb.control(tag, [Validators.maxLength(30)]));
+        });
+      });
+    });
   };
 
   private createForm(): FormGroup {
     return this.fb.group({
-      title: [this.taskStore.taskSelected()?.title, [Validators.required, Validators.maxLength(50), Validators.minLength(3)]],
-      status: [this.taskStore.taskSelected()?.status, [Validators.required]],
-      description: [this.taskStore.taskSelected()?.description, [Validators.required, Validators.maxLength(255), Validators.minLength(10)]],
-      priority: [this.taskStore.taskSelected()?.priority, [Validators.required, Validators.maxLength(20)]],
-      category: [this.taskStore.taskSelected()?.category, [Validators.required, Validators.maxLength(50), Validators.minLength(2)]],
-      tags: [[]]
+      title: ["", [Validators.required, Validators.maxLength(50), Validators.minLength(3)]],
+      status: ["", [Validators.required]],
+      description: ["", [Validators.required, Validators.maxLength(255), Validators.minLength(10)]],
+      priority: ["", [Validators.required, Validators.maxLength(20)]],
+      category: ["", [Validators.required, Validators.maxLength(50), Validators.minLength(2)]],
+      tags: this.fb.array([])
     });
   };
 
   protected onSubmit() {
-    this.taskStore.update(this.form.value, this.taskStore.taskSelected()?.id)
+    this.taskStore.update(this.form.value, this.task()?.id)
       .then(({ message }) => this.toastr.success(message) && (this.form.reset(), this.router.navigate(['/'])))
       .catch(({ error: { errors, message }}) => this.toastr.error(message) && this.errors.set(errors));
   };
 
   protected onDelete = () => {
-    this.taskStore.delete(this.taskStore.taskSelected()?.id)
+    this.taskStore.delete(this.task()?.id)
       .then(({ message }) => this.toastr.success(message) && (this.form.reset(), this.router.navigate(['/'])))
       .catch(({ error: { errors, message }}) => this.toastr.error(message) && this.errors.set(errors));
   };
